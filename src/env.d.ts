9713dddcb82a46b5b5d0d6c82078bcd5b434fa8a
Ivan Angelov <ivanangelov414@gmail.com>
2025-10-25 16:53:20 +0300

    Fixes/refactorings

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,15 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 /// <reference types="vite/client" />
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-type ImportMeta = {
-	readonly env: {
-		readonly VITE_API_URL: string;
-		readonly VITE_API_VERSION: string;
-		readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
-	};
-};
+interface ImportMetaEnv {
+	readonly VITE_API_URL: string;
+	readonly VITE_API_VERSION: string;
+	readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
 
 declare global {
 	namespace NodeJS {
@@ -15,5 +17,3 @@ declare global {
 		type ProcessEnv = {};
 	}
 }
-
-export {};
--- src/lib/api/params.ts
+++
@@ -1,6 +0,0 @@
-export const objectToSearchParams = (obj: object) =>
-	new URLSearchParams(
-		Object.entries(obj)
-			.filter(([, value]) => value !== undefined && value !== null)
-			.map(([key, value]) => [key, String(value)]),
-	).toString();
