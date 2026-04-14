fa2fa027df1c902930cb8364fc731e6b51bd6c04
Ivan Angelov <ivanangelov414@gmail.com>
2026-04-14 13:15:02 +0300

    Forgot to define a translation hook

--- src/app/hooks/locales/translations/common.ts
+++ src/app/hooks/locales/translations/common.ts
@@ -2,6 +2,8 @@ import { useTranslation } from '../useTranslation';
 
 export const useLocalesTranslations = () => useTranslation('common.locales').t;
 
+export const useLoadingTranslations = () => useTranslation('common.loading').t;
+
 export const useEmptyTranslations = () => useTranslation('common.empty').t;
 
 export const useErrorsTranslations = () => useTranslation('common.errors').t;
