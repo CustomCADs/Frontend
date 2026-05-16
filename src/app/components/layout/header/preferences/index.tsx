2cb11fbf43489c578684f81503f79ee42ba22484
Ivan Angelov <ivanangelov414@gmail.com>
2026-05-16 03:41:11 +0300

    Changed 'Preferences' tab to appear via Shadcn/UI Sheet instead of Dialog

--- src/app/components/layout/header/preferences/index.tsx
+++ src/app/components/layout/header/preferences/index.tsx
@@ -2,50 +2,42 @@ import { Settings2 } from 'lucide-react';
 import { cn } from '@/lib/utils/tailwindcss';
 import { useInfoTranslations } from '@/app/hooks/locales/translations/pages/public';
 import CustomIcon from '@/app/components/icon';
-import * as dialog from '@/app/components/ui/dialog';
+import * as sheet from '@/app/components/ui/sheet';
 import * as preferences from '@/app/components/preferences';
 
 const Preferences = ({ text }: { text?: string }) => {
 	const tPreferences = useInfoTranslations('preferences');
 
 	return (
-		<dialog.Dialog>
-			<dialog.DialogTrigger>
+		<sheet.Sheet>
+			<sheet.SheetTrigger>
 				<CustomIcon Icon={Settings2} text={text} />
-			</dialog.DialogTrigger>
-			<dialog.DialogContent
+			</sheet.SheetTrigger>
+			<sheet.SheetContent
 				className={cn(
 					'w-5/6 md:w-auto py-20 md:p-16 rounded-3xl',
-					'flex flex-col justify-center items-center',
-					'bg-accent text-accent-foreground',
+					'flex flex-col justify-center items-center gap-y-12',
+					'bg-accent text-accent-foreground text-lg',
 				)}
 			>
-				<div>
-					<div className='flex flex-col gap-y-8 text-lg'>
-						<div className='flex gap-x-4'>
-							<span>{tPreferences('theme')}</span>
-							<preferences.ThemeToggle />
-						</div>
-						<div className='flex gap-x-4'>
-							<span>{tPreferences('language')}</span>
-							<preferences.LanguageMenu
-								placeholder={tPreferences(
-									'language-placeholder',
-								)}
-							/>
-						</div>
-						<div className='flex gap-x-4'>
-							<span>{tPreferences('currency')}</span>
-							<preferences.CurrencySelector
-								placeholder={tPreferences(
-									'currency-placeholder',
-								)}
-							/>
-						</div>
-					</div>
+				<div className='flex gap-x-4'>
+					<span>{tPreferences('theme')}</span>
+					<preferences.ThemeToggle />
 				</div>
-			</dialog.DialogContent>
-		</dialog.Dialog>
+				<div className='flex gap-x-4'>
+					<span>{tPreferences('language')}</span>
+					<preferences.LanguageMenu
+						placeholder={tPreferences('language-placeholder')}
+					/>
+				</div>
+				<div className='flex gap-x-4'>
+					<span>{tPreferences('currency')}</span>
+					<preferences.CurrencySelector
+						placeholder={tPreferences('currency-placeholder')}
+					/>
+				</div>
+			</sheet.SheetContent>
+		</sheet.Sheet>
 	);
 };
 
