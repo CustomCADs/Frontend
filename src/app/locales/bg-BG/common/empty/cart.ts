c44417b39f82f981c6dca7533ea5ab81af1084b5
Ivan Angelov <ivanangelov414@gmail.com>
2026-08-18 05:33:25 +0300

    Fixed build errors

--- src/app/locales/bg-BG/common/empty/cart.ts
+++
@@ -1,7 +0,0 @@
-import { Empty } from '@/app/locales/types/common/empty';
-
-export default {
-	title: 'Няма добавени Продукти все още.',
-	description: 'Можеш да допринесеш като се регистрираш и качиш моделите си!',
-	link: 'Добави Продукти',
-} satisfies Empty['common.empty.products'];
--- src/app/locales/en-GB/common/empty/cart.ts
+++
@@ -1,8 +0,0 @@
-import { Empty } from '@/app/locales/types/common/empty';
-
-export default {
-	title: 'No added Products yet.',
-	description:
-		'You can fill your Cart by exploring our Gallery and adding any Product!',
-	link: 'Add Products to Cart',
-} satisfies Empty['common.empty.products'];
